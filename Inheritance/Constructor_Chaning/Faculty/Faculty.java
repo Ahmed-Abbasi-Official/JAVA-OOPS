6d26005d0faabaccc9d33236bdd28b4104902391
Ahmed0312006 <farhad.khi17@gmail.com>
2024-09-10 22:41:12 +0500

    new Work

---
+++ Inheritance/Constructor_Chaning/Faculty/Faculty.java
@@ -0,0 +1,12 @@
+package Constructor_Chaning.Faculty;
+
+public class Faculty extends Employee {
+    public static void main(String[] args) {
+        Faculty a=new Faculty();
+        System.out.println("aaaaaaaa"+a);
+    }
+
+    public Faculty() {
+         System.out.println("(4) Performs Faculty's tasks");
+        }
+}
---
+++ Inheritance/Constructor_Chaning/Faculty/Person.java
@@ -0,0 +1,7 @@
+package Constructor_Chaning.Faculty;
+
+class Person {
+    public Person() {
+        System.out.println("(1) Performs Person's tasks");
+    }
+}
